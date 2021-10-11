5426ab0361bca12b369c2c2a3696d79b6075358b
Feri73 <ferisampad@gmail.com>
2021-10-11 12:44:18 -0400

    adds rec_update to Config

--- src/scilib/base/configuration.py
+++ src/scilib/base/configuration.py
@@ -2,3 +2,15 @@ class Config(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
+    def rec_update(self, other: 'Config') -> 'Config':
+        for k in other:
+            if k in self:
+                if isinstance(self[k], Config):
+                    self[k] = self[k].rec_update(other[k])
+                else:
+                    self[k] = other[k]
+            else:
+                self[k] = other[k]
+
+        return self
