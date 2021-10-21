01b9a9f6b9fe441df318c60513a540c715c78217
Feri73 <ferisampad@gmail.com>
2021-10-21 17:54:39 -0400

    adds keydefaultdict moves config to utils

--- src/scilib/utils.py
+++ src/scilib/utils.py
@@ -1,6 +1,38 @@
 import importlib.util
+from collections import defaultdict
 from types import ModuleType
 from typing import Union, Any
+
+
+class Config(dict):
+    def __getattr__(self, item):
+        if item in self:
+            return self[item]
+        return super(Config, self).__getattr__(item)
+
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def rec_update(self, other: 'Config') -> 'Config':
+        for k in other:
+            if k in self:
+                if isinstance(self[k], Config):
+                    self[k] = self[k].rec_update(other[k])
+                else:
+                    self[k] = other[k]
+            else:
+                self[k] = other[k]
+
+        return self
+
+
+class KeyDefaultDict(defaultdict):
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        else:
+            ret = self[key] = self.default_factory(key)
+            return ret
 
 
 def do_import(file: str, element: str = None, name='_tmp_') -> Union[ModuleType, Any]:
