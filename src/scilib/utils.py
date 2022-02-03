6139d95742ae30a7fa172f55b3bca768854ca2f7
Feri73 <ferisampad@gmail.com>
2022-02-03 18:54:50 -0500

    solves typing bug

--- src/scilib/utils.py
+++ src/scilib/utils.py
@@ -45,7 +45,7 @@ def do_import(file: str, element: str = None, name='_tmp_') -> Union[ModuleType,
         return getattr(file, element)
 
 
-def load_pickle(pickle, file_name: str) -> None:
+def load_pickle(pickle, file_name: str):
     with open(file_name, 'rb') as f:
         return pickle.load(f)
 
