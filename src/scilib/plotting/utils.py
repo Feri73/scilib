8de0fb27d515f1a61f9e8f52da4a726f4c8a51aa
Feri73 <ferisampad@gmail.com>
2023-01-16 16:17:25 -0500

    adds PlotMemory to plotting utils

--- src/scilib/plotting/utils.py
+++ src/scilib/plotting/utils.py
@@ -1,6 +1,35 @@
-from typing import Iterable, Dict, Any
+from typing import Iterable, Dict, Any, Callable, List
 import numpy.typing as npt
 import matplotlib
+from collections import defaultdict
+
+
+class PlotMemory:
+    def __init__(self, func: Callable, arg_cache_inds: List[int] = None, kwarg_cache_keys: List[str] = None):
+        self.__func = func
+        self.__arg_cache_inds = arg_cache_inds
+        self.__kwarg_cache_keys = kwarg_cache_keys
+        assert arg_cache_inds is not None or kwarg_cache_keys is not None
+        self.__arg_cache = defaultdict(list)
+        self.__kwarg_cache = defaultdict(list)
+
+    def __call__(self, *args, **kwargs):
+        if self.__arg_cache_inds is not None:
+            for i in self.__arg_cache_inds:
+                self.__arg_cache[i].append(args[i])
+        if self.__kwarg_cache_keys is not None:
+            for k in self.__kwarg_cache_keys:
+                self.__kwarg_cache[k].append(kwargs[k])
+
+        return self.__func(*args, **kwargs)
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return self.__arg_cache[item]
+        elif isinstance(item, str):
+            return self.__kwarg_cache[item]
+        else:
+            raise TypeError(f'item must be int or str, not {type(item)}')
 
 
 def get_colors(elements: Iterable, cname='gist_rainbow') -> Dict[Any, npt.NDArray]:
